3ddebcbb6c1d3a10ba5e5122790b4b277feaf975
maxwellsdemon <Fastomchik@gmail.com>
2025-09-13 01:45:33 +0300

    added btn in homepage

--- homepage.cpp
+++ homepage.cpp
@@ -1,10 +1,40 @@
 #include "homepage.h"
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QGridLayout>
+#include <QLabel>
+
+HomeWidget::HomeWidget(QWidget *parent) : QWidget(parent)
+{
+
+}
+
+
 
 HomePage::HomePage(QWidget *parent) : QWidget(parent)
 {
-    QVBoxLayout *layout = new QVBoxLayout(this);
-    QLabel *label = new QLabel("Главная страница\nДобро пожаловать в систему мониторинга");
-    label->setAlignment(Qt::AlignCenter);
-    label->setStyleSheet("font-size: 16px;");
-    layout->addWidget(label);
+    setupUI();
+}
+
+void HomePage::setupUI()
+{
+    QVBoxLayout *mainLayot = new QVBoxLayout(this);
+    mainLayot->setSpacing(20);
+    mainLayot->setContentsMargins(30,30,30,30);
+
+    QLabel *titleLabel = new QLabel("Статус подключения");
+    titleLabel->setAlignment(Qt::AlignCenter);
+    titleLabel->setStyleSheet("font-size: 20px; font-weight: bold; color: #2c3e50; margin-bottom: 20px;");
+    mainLayot->addWidget(titleLabel);
+
+    QGridLayout *gridLayot = new QGridLayout;
+    gridLayot->setSpacing(30);
+    gridLayot->setContentsMargins(0,0,0,0);
+
+    btnStartServer = new QPushButton("Старт сервера");
+    btnConnectClient = new QPushButton("Подключиться к клиенту");
+    gridLayot->addWidget(btnStartServer, 0, 0);
+    gridLayot->addWidget(btnConnectClient, 0, 1);
+    mainLayot->addLayout(gridLayot);
+    mainLayot->addStretch();
 }
--- homepage.h
+++ homepage.h
@@ -2,14 +2,29 @@
 #define HOMEPAGE_H
 
 #include <QWidget>
-#include <QLabel>
-#include <QVBoxLayout>
+#include <QPushButton>
+
+class HomeWidget : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit HomeWidget(QWidget *parent = nullptr);
+};
+
 
 class HomePage : public QWidget
 {
     Q_OBJECT
 public:
     explicit HomePage(QWidget *parent = nullptr);
+private:
+    void setupUI();
+    void updateDisplay();
+
+    // Кнопки
+    QPushButton *btnStartServer;
+    QPushButton *btnConnectClient;
+
 };
 
 #endif // HOMEPAGE_H
